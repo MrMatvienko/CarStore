b1148143451a02cc999b689e7da784270b99b163
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2025-05-23 12:39:46 +0300

    bugfix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
-
+import path from "path";
 export default defineConfig({
   base: "/CarStore/",
-  plugins: [mp()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        admin: path.resolve(__dirname, "admin.html"),
+      },
+    },
+  },
 });
