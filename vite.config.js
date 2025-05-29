881792ff68866fbfdc56233a03984617b4e80068
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2025-05-29 15:07:56 +0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
-import path from "path";
+import { resolve } from "path";
 export default defineConfig({
   base: "/CarStore/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        admin: resolve(__dirname, "admin/index.html"),
+        ua: resolve(__dirname, "ua/index.html"),
+        ru: resolve(__dirname, "ru/index.html"),
+      },
+    },
+  },
 });
