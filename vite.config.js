84340d1cfdfecedec55208263f8f95413a315bdb
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2025-05-29 14:28:50 +0300

    add language version

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,4 @@ import { defineConfig } from "vite";
 import path from "path";
 export default defineConfig({
   base: "/CarStore/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-        admin: path.resolve(__dirname, "admin.html"),
-      },
-    },
-  },
 });
