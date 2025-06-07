2dfd5b2b25800bf5708ee49712717b4d7e6e2063
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2025-06-07 19:05:44 +0300

    drop

--- src/main.js
+++ src/main.js
@@ -5,14 +5,21 @@ const selectLists = document.querySelectorAll(".header_select-list");
 arrowSelects.forEach((btn, index) => {
   let isOpen = false;
 
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation(); // Щоб клік не доходив до document
+
     const container = selectContainers[index];
-    const list = selectLists[index];
 
     if (isOpen) {
       container.style.height = "24px";
       container.style.background = "transparent";
     } else {
+      // Закриваємо всі інші
+      selectContainers.forEach((el) => {
+        el.style.height = "24px";
+        el.style.background = "transparent";
+      });
+
       container.style.height = "74px";
       container.style.background = "var(--white-500)";
     }
@@ -21,6 +28,12 @@ arrowSelects.forEach((btn, index) => {
   });
 });
 
+document.addEventListener("click", () => {
+  selectContainers.forEach((container) => {
+    container.style.height = "24px";
+    container.style.background = "transparent";
+  });
+});
 /*-------burger-------*/
 
 const burgerBtn = document.querySelector(".header_burger");
