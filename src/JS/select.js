5e911e1aed21a570ff90598c976067b6013da3d0
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2025-06-17 14:59:37 +0300

    backdrop select list

--- src/JS/select.js
+++ src/JS/select.js
@@ -3,10 +3,27 @@ export function selectFiller() {
   const selectBox = document.querySelector(".car_checkbox");
   const fillterBtn = document.querySelector(".car_fillter-text");
 
-  arrowBtn.addEventListener("click", () => {
+  // Перевіряємо, чи всі елементи існують у DOM
+  if (!arrowBtn || !selectBox || !fillterBtn) {
+    console.error("One or more elements for selectFiller not found!");
+    return;
+  }
+  const toggleSelectBox = () => {
     selectBox.classList.toggle("show");
-  });
-  fillterBtn.addEventListener("click", () => {
-    selectBox.classList.toggle("show");
+  };
+  arrowBtn.addEventListener("click", toggleSelectBox);
+  fillterBtn.addEventListener("click", toggleSelectBox);
+  document.addEventListener("click", (event) => {
+    const isClickInsideSelectBox = selectBox.contains(event.target);
+    const isClickOnToggleButtons =
+      arrowBtn.contains(event.target) || fillterBtn.contains(event.target);
+
+    if (
+      selectBox.classList.contains("show") &&
+      !isClickInsideSelectBox &&
+      !isClickOnToggleButtons
+    ) {
+      selectBox.classList.remove("show");
+    }
   });
 }
